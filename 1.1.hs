6688f677e7191d3a951c4413c176d46921885897
Giulio Starace <giulio.starace@gmail.com>
2019-05-16 17:43:21 +0100

    complete 1e, fix issue with 1d

--- 1.1.hs
+++ 1.1.hs
@@ -14,10 +14,20 @@ n = a `div` length xs
 -- c)
 {-| Write the library function last, which selects the last element of a list,
 using the functions introduced in the slides.-}
-last :: [a] -> a
-last (x:xs) = head (reverse xs)
+last' :: [a] -> a
+last' xs = head (reverse xs)
 
 -- d)
 -- Can you think of another definition?
-last :: [a] -> a
-last (x:xs) = head . traverse
+last'' :: [a] -> a
+last'' xs = xs !! (length xs - 1)
+
+-- e)
+{-| Write the library function init, which removes the last element from a list,
+ in two different ways.-}
+-- first way
+init' :: [a] -> [a]
+init' xs = reverse (drop 1 (reverse xs))
+-- second way
+init'' :: [a] -> [a]
+init'' xs = take (length xs-1) xs
