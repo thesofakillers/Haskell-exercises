321bffcdd6a4a2f7729f102967a924e2654892b4
Giulio Starace <giulio.starace@gmail.com>
2019-05-17 21:27:32 +0100

    only unique triples

--- 3/3.2.hs
+++ 3/3.2.hs
@@ -4,4 +4,4 @@ Using a list comprehension, define pyths :: Int -> [(Int, Int, Int)] that maps
 an integer n to all such triples with components in [1..n]
 -}
 pyths :: Int -> [(Int, Int, Int)]
-pyths n = [(x, y, n) | y <- [1..n-1], x <- [1..n-1], x^2 + y^2 == n^2]
+pyths n = [(x, y, n) | y <- [1..n-1], x <- [1..y], x^2 + y^2 == n^2]
