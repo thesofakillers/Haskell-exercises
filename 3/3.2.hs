b11c5f0c323125cf8bcf6d44967b000c7d3bc114
Giulio Starace <giulio.starace@gmail.com>
2019-05-17 21:26:12 +0100

    complete 3.2

---
+++ 3/3.2.hs
@@ -0,0 +1,7 @@
+{-
+A triple (x, y, z) of positive integers is called pythagorean if x^2 +y^2 = z^2.
+Using a list comprehension, define pyths :: Int -> [(Int, Int, Int)] that maps
+an integer n to all such triples with components in [1..n]
+-}
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(x, y, n) | y <- [1..n-1], x <- [1..n-1], x^2 + y^2 == n^2]
